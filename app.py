3b9552b3c196a4e9c6bdca481d5811c3ccc6e0f1
Radji Mohamed <radjimohamed013@gmail.com>
2025-01-25 18:20:02 +0100

    ajoute d'une base de donées

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask , render_template , request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)  
 
 
-# listes des taches
-tasks = ['task1','task2','task3','task4','task5']
+#configuraton de la base de données
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///taskt.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#création de la table taches
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'Task : {self.id}: {self.task} {self.completed}'
 
 
 
@@ -13,16 +28,46 @@ def home():
     if request.method == 'POST':
         new_task = request.form['task']
         if new_task:
-            tasks.append(new_task)
+            task = Task(task=new_task)
+            db.session.add(task)
+            db.session.commit()
+    tasks = Task.query.all()
+
+
+
+
+    #gestion des recherche et filtrage des taches
+    search = request.args.get('search', "")
+    filtrage = request.args.get('filter', 'all')
+    if filtrage == 'completed':
+        tasks = Task.query.filter_by(completed=True).all()
+    elif filtrage == 'not_completed':
+        tasks = Task.query.filter_by(completed=False).all()
+    else:
+        tasks = Task.query.all()
     return  render_template('home.html', tasks = tasks)
 
 
-#supression d'une taches
-@app.route('/delete', methods=['POST'])
-def delete_task():
-    task_delete = request.form.get('task_delete')
-    tasks.remove(task_delete)
-    return redirect(url_for('home'))
+    
+
+
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect('/')
+
+
+
+@app.route('/complete/<int:task_id>')
+def complete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    task.completed = not task.completed
+    db.session.commit()
+    return redirect('/')
+
 
 
 
